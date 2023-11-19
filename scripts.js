93f700f3922d5bb3cccdb322e2d62930968af018
Mararovsek99 <andrej.marovsek99@gmail.com>
2023-11-19 20:56:53 +0100

    added fb logo and ig logo with linkable page

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.querySelector(".fbLogo").addEventListener("click", function(){
+    window.location.href = "https://www.facebook.com/profile.php?id=61553308209404";
+});
+document.querySelector(".igLogo").addEventListener("click", function(){
+    window.location.href = "https://www.instagram.com/fizio_luma/";
+});

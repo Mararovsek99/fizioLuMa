c97b3e7837fda0b802ffa072677f31c310a6ab06
Mararovsek99 <andrej.marovsek99@gmail.com>
2023-11-25 19:26:47 +0100

    added navigation buttons

--- scripts.js
+++ scripts.js
@@ -7,11 +7,11 @@ document.querySelector(".igLogo").addEventListener("click", function(){
 
 window.addEventListener("scroll",function(){
     const navBarOpacity = document.querySelector(".navBar");
-    const maxScroll =100;
+    const maxScroll =200;
 
-    const opacity = Math.min(1, maxScroll / window.scrollY);
+    const opacity = Math.min(1, window.scrollY / maxScroll + 0.4);
 
-    navBarOpacity.style.backgroundColor = `rgba(241,233,222, ${opacity})`;
+    navBarOpacity.style.backgroundColor = `rgba(241,233,222, ${opacity - 0.1})`;
 
 
 });

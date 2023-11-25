cf3b6b463b2499824f7d46539852338f24731a59
Mararovsek99 <andrej.marovsek99@gmail.com>
2023-11-25 18:00:39 +0100

    added maini page photo and changed header

--- scripts.js
+++ scripts.js
@@ -4,3 +4,14 @@ document.querySelector(".fbLogo").addEventListener("click", function(){
 document.querySelector(".igLogo").addEventListener("click", function(){
     window.location.href = "https://www.instagram.com/fizio_luma/";
 });
+
+window.addEventListener("scroll",function(){
+    const navBarOpacity = document.querySelector(".navBar");
+    const maxScroll =100;
+
+    const opacity = Math.min(1, maxScroll / window.scrollY);
+
+    navBarOpacity.style.backgroundColor = `rgba(241,233,222, ${opacity})`;
+
+
+});
